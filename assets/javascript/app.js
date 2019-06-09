5e1143624ee985122122a66db172a4afac0ed3fe
birna <briandam26@yahoo.com>
2019-06-09 11:52:15 -0700

    pseudocoded the logic behind the game

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    //firebase config info
+
+        //player class to hold player data 
+            //(name, wins, losses, ties, last move)
+
+        //message class to create objects for each msg
+            //(who sent the message and what did they say)
+
+        //database location for current players
+        //database location for chat messagges
+
+        //function to take in new player when "enter" is clicked
+            //have user re-enter a name if field is left blank
+            //valid name -> insert player into database and make UI changes
+
+        //function to take in player's RPS choice
+            //store the player's moves
+            //check if both players have went
+            //get result
+            //update UI with the correct changes
+
+        //chatroom functionality
+            //prevent page refresh
+            //show msges on interface once sent
+            //prevent blank messages
+            //make message object
+            //clear msg box in UI
+        
+        //function to update player data
+
+        //function to enable RPS buttons when there are two players
+
+        //function to calculate who won based on "last move" property
+
+        //function to update results, wins, losses, and ties in UI
+});
