21428fc83c65caf25fcfe75d421f13abc199b34e
birna <briandam26@yahoo.com>
2019-06-10 01:10:18 -0700

    added functionality between user input and change to UI for name

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,67 +1,120 @@
-$(document).ready(function(){
-
+rps = {
     //firebase config info
-    const fbConfig = {
+    fbConfig: {
         apiKey: "AIzaSyDO1LjkDTI1kHBokq5OoLnSTMxg-4m14AM",
         authDomain: "birnarpsgame.firebaseapp.com",
         databaseURL: "https://birnarpsgame.firebaseio.com",
         projectId: "birnarpsgame",
         storageBucket: "birnarpsgame.appspot.com",
-        messagingSenderId: "25115998215"
-    };
-    
+        messagingSenderId: "25115998215",
+        appId: "1:25115998215:web:dea6baf4c9fe2a49"
+    },
+
     //variables 
-    let database = "";
-    let numPlayers = 0;
-    let playerDes = "";
-    let playersDB = "";
-    let msgesDB = "";
+    database: "",
+    numPlayers: 0,
+    playerDes: "",
+    playersDB: "",
+    msgesDB: "",
+    //need this to hold empty space
+    players: { player1: null, player2: null },
 
     //player class to hold player data 
         //(name, wins, losses, ties, last move)
-    let player = function(name) {
+    player: function(name) {
         this.name = name;
         this.wins = 0;
         this.losses = 0;
         this.ties = 0;
         this.lastMove = "";
-    };
+    },
 
     //message class to create objects for each msg
         //(who sent the message and what did they say)
-    let message = function(chat, sender = 'anonymous') {
+    message: function(chat, sender = 'anonymous') {
         if(sender!='admin'){
-            if (playerDes != ""){
-                sender = playerDes;
+            if (rps.playerDes != ""){
+                sender = rps.playerDes;
             }
         }
         this.sender = sender
         this.message = chat;
         this.time = new Date().toLocaleTimeString();
         console.log(this.sender +":"+ chat);
-    };
+    },
         
     //initialize the app with firebase to be able to store information
-    let init = function() {
-        firebase.initializeApp(fbConfig);
+    init: function() {
+        firebase.initializeApp(rps.fbConfig);
         database = firebase.database();
         //database location for current players
         playersDB = database.ref('players/');        
         //database location for chat messagges
         msgesDB = database.ref('messages/');     
-    };
-    //run the function    
-    init();
+        rps.clickHandlers();
+    },
+    
+    //function to carry out all click events
+    clickHandlers: function() {
 
-        //function to take in new player when "enter" is clicked
-            //have user re-enter a name if field is left blank
+        //function to take in new player when "join" is clicked
+        $(".ready").on("click", function(b) {
+            let curPlayer = $(b.target).attr("data-player");
+            let enteredTxt = $('#' + curPlayer + ' .name').val().trim();
+            //have user re-enter name if field is left blank
+            if (enteredTxt == "") {
+                alert("Please enter a valid name")
+            } else {
             //valid name -> insert player into database and make UI changes
+            rps.playerDes = curPlayer;
+            rps.players[rps.playerDes] = new rps.player(enteredTxt);
+            rps.updatePlayers();
+            let connectionsRef = database.ref("/players/"+rps.playerDes);
+            connectionsRef.set(rps.players[rps.playerDes]);
+            //remove player from DB if they disconnect
+            connectionsRef.onDisconnect(   
+            function () {
+                msgesDB.push(new rps.message(enteredTxt +
+                " has left the building.", 'admin'));
+            }).remove();
+            $('.name').attr('disabled', true);  
+            }
+        });
 
         //function to take in player's RPS choice
             //store the player's moves
             //check if both players have went
             //get result
             //update UI with the correct changes
+
+    },
+
+    //function to update player data
+    updatePlayers: function() {
+        $('.player-title').html("Player Name: ")
+        //show name input
+        $(' .form-group').slideDown();   
+
+        //update UI with all player info and disable RPS buttons
+        for (curPlayer in rps.players){
+            if (rps.players[curPlayer] != null){
+                $('#' + curPlayer + ' .player-title').html("Player Name: " + 
+                    rps.players[curPlayer].name);
+
+                $('#' + curPlayer + ' .form-group').slideUp();
+                $('.move-text').html("Waiting for another player");
+                $('.moves :button').attr('disabled', true);   
+            }
+             
+            //names on scoreboard
+            if (rps.players.player1 != null){
+                $(".p1").text(rps.players.player1.name);
+            }
+            if (rps.players.player2 != null){
+                $(".p2").text(rps.players.player2.name);
+            }
+        }
+    },
 
         //chatroom functionality
             //prevent page refresh
@@ -70,11 +123,16 @@ $(document).ready(function(){
             //make message object
             //clear msg box in UI
         
-        //function to update player data
 
         //function to enable RPS buttons when there are two players
 
         //function to calculate who won based on "last move" property
 
         //function to update results, wins, losses, and ties in UI
+               
+}
+
+//run the rps Object
+$(document).ready(function() {
+    rps.init();
 });
